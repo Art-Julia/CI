a7f7bb763f012f11a9d84f79a0261456c77f6537
Yulia Arutyunyan <artn.julia@gmail.com>
2021-06-15 22:34:44 +0300

    CI finally is green

--- src/main/java/ru/netology/statistics/StatisticsService.java
+++ src/main/java/ru/netology/statistics/StatisticsService.java
@@ -11,17 +11,65 @@ public class StatisticsService  {
         long current_max_index = 0;
         long current_max = incomes[0];
         for (long income : incomes) {
+            if (current_max < income) {
+                current_max = income;
+            }
+        }
+        return current_max;
+    }
+    public long findMax12_(long[] incomesInBillions) {
+//        long current_max_index = 0;
+        long current_max = incomesInBillions[0];
+        for (long income : incomesInBillions) {
             if (current_max <= income) {
                 current_max = income;
             }
         }
         return current_max;
     }
+
+    public long findMaxMonth(long[] incomesInBillions) {
+//        long current_max_index = 0;
+        long current_max = incomesInBillions[0];
+        int maxMonth = 0;
+        int month = 0;
+        for (long income : incomesInBillions) {
+            if (current_max <= income) {
+                maxMonth++;
+            }
+            month = month +1;
+        }
+        return maxMonth;
+    }
+    public long findMaxIncome(long[] incomesInBillions) {
+//        long current_max_index = 0;
+        long current_max = 0;
+//        int maxMonth = 0;
+        int month = 0;
+        for (long income : incomesInBillions) {
+            if (current_max <= income) {
+                current_max = income;
+            }
+            month = month +1;
+        }
+        return current_max;
+    }
+
+    public long findMax12(long[] incomes) {
+        long current_max_index = 0;
+//        long current_max = incomes[0];
+        for (long income : incomes) {
+            if (current_max_index <=income) {
+                current_max_index = income;
+            }
+        }
+        return current_max_index;
+    }
     public long findMin(long[] incomes) {
         long current_max_index = 0;
         long current_min = incomes[0];
         for (long income : incomes) {
-            if (current_min > income) {
+            if (current_min >= income) {
                 current_min = income;
             }
         }
